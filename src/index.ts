954216453786b364fb59d1d1e4a73bcb4552821d
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-06-17 19:31:21 +0530

    feat: add express server with security and CORS middleware
    - Added express, helmet, cors, and cookie-parser as dependencies.
    - Configured dotenv for environment variable management.
    - Implemented middleware for HTTPS redirection and CORS support.
    - Set up JSON and URL-encoded body parsing.
    - Created a basic server listening on a specified port.

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { configDotenv } from "dotenv";
+
+const app = express();
+
+configDotenv()
+app.use((req, res, next) => {
+  if (req.headers["x-forwarded-proto"] === "http") {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+    return;
+  }
+  next();
+});
+app.use(helmet());
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

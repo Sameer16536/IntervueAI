c96e812840fe5404aecb8e29296a1981d7e41a57
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-06-20 22:42:34 +0530

    feat: add login functionality and improve user registration process

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import rateLimit from "express-rate-limit";
 import jwt, { decode, JwtPayload } from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { userInputValidation } from "../utils/types";
+import { loginInputValidation, userInputValidation } from "../utils/types";
 
 const prisma = new PrismaClient();
 
@@ -68,20 +68,45 @@ const registerUser = async (
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-    //send refresh token as cookie
+    //send refresh token as cookie ??
     res.status(201).json({
       message: "User Created successfully",
       user,
       accessToken,
       refreshToken,
     });
+    return;
   } catch (err) {
     console.error("Error in Register User", err);
   }
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const validateInput = loginInputValidation.parse(req.body);
+    const { email, password } = validateInput;
 
+    //Check if user exists
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (
+      !existingUser ||
+      !existingUser.password ||
+      (await bcrypt.compare(password, existingUser.password))
+    ) {
+      res.status(401).json({ message: "Invalid Credentials" });
+      return;
+    }
+
+    const accessToken = generateAccessToken(existingUser)
+    const refreshToken = generateRefreshToken(existingUser)
+
+  } catch (err) {
+    console.error("Error while Login", err);
+  }
+};
 
 module.exports = {
-  registerUser
-}
+  registerUser,
+  loginUser,
+};
